cb6c8510efb11c848ddda92b2d80a0ab93b8aaf2
Hannah Olbrich <hannaholbrich@gmail.com>
2024-11-07 16:59:33 +0000

    Add product_detail functionality

--- ready_steady_eat/settings.py
+++ ready_steady_eat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ready-steady-eat-8febfd678f9f.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ready-steady-eat-8febfd678f9f.herokuapp.com', '8000-hannahro15-readysteadye-pmisdgf4v8l.ws.codeinstitute-ide.net', 'localhost']
 
 # Application definition
 
