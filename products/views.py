d62c74dac91dda1ea99f184226d47be860db2c27
Hannah Olbrich <hannaholbrich@gmail.com>
2024-11-08 13:41:00 +0000

    Add category to imports in views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
