2da9077ff0aace5b34e76d3b78141118ce209c2a
Hannah Olbrich <hannaholbrich@gmail.com>
2024-11-25 18:13:13 +0000

    Add checkout form data caching in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -18,6 +18,9 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
+        
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
