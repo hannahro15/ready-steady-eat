c6f90590635c08c06b3cd23db84d738ffd807489
Hannah Olbrich <hannaholbrich@gmail.com>
2024-11-27 19:10:29 +0000

    make contact and faq separate links

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
    path('contact/', views.contact, name='contact'),
+   path('faq/', views.faq, name='faq'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def contact(request):
     return render(request, 'contact/contact.html')
+
+
+def faq(request):
+    return render(request, 'contact/faq.html')
