022cc61534bb6d628473dea4fda576a6c145e99a
Hannah Olbrich <hannaholbrich@gmail.com>
2024-11-28 17:55:21 +0000

    add admin to faq page

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Faq
 
 # Register your models here.
+class Faq(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'answer',
+    )
