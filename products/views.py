3efe49ab45947c8937471f4095627a2a87a9c2df
Hannah Olbrich <hannaholbrich@gmail.com>
2024-12-06 16:19:57 +0000

    Fix url errors links and amend navbar

--- products/views.py
+++ products/views.py
@@ -78,13 +78,8 @@ def add_product(request):
     return render(request, template, context)
 
 
-@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
