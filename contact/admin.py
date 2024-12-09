3eb9d81c42db91a98f9d61967afe51851039bfec
Hannah Olbrich <hannaholbrich@gmail.com>
2024-12-09 16:18:13 +0000

    Add contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Faq
+from .models import Contact
 
 # Register your models here.
 admin.site.register(Faq)
+admin.site.register(Contact)
--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,13 @@ from django.conf import settings
 
 # Create your models here.
 
+class Contact(models.Model):
+        firstName = models.CharField(max_length=250, null=False, blank=True)
+        lastName = models.CharField(max_length=250, null=False, blank=True)
+        email = models.EmailField(max_length=250, null=False, blank=True)
+        phone = models.IntegerField(null=False, blank=True)
+        comments = models.TextField(max_length=250, null=False, blank=True)
+
 class Faq(models.Model):
         question = models.CharField(max_length=250, null=False, blank=True)
         answer = models.TextField(max_length=250, null=False, blank=True)
