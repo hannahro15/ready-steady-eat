81f0272f7c97ab37524436f2e5d7010161582bdd
Hannah Olbrich <hannaholbrich@gmail.com>
2024-12-09 17:28:16 +0000

    Start implementing some crud functionality on faq page

--- contact/urls.py
+++ contact/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
    path('contact/', views.contact, name='contact'),
    path('faq/', views.faq_page, name='faq_page'),
+   path('edit/<int:faq_id>/', views.faq_page, name='faq_page'),
+   path('delete/<int:faq_id>/', views.faq_page, name='faq_page')
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, get_object_or_404
 from .models import Faq
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -11,30 +12,40 @@ def faq_page(request):
     faq_list = Faq.objects.all()
     return render(request, 'contact/faq.html', {'faq_list': faq_list})
 
-@login_required
-def edit_product(request, faq_list):
-    """ Edit FAQ in the store """
 
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
-    product = get_object_or_404(Faq, pk=faq_id)
+def edit_faq(request, faq_id):
+    """ Edit an faq """
+    faq = get_object_or_404(Faq, pk=faq_id)
     if request.method == 'POST':
-        form = Faq(request.POST, request.FILES, instance=product)
+        form = FaqForm(request.POST, request.FILES, instance=faq)
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+            return redirect(reverse('faq_page', args=[faq.id]))
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
-        messages.info(request, f'You are editing {product.name}')
+        form = FaqForm(instance=faq)
+        messages.info(request, f'You are editing {faq.id}')
 
     template = 'products/edit_product.html'
     context = {
         'form': form,
-        'product': product,
+        'faq': faq,
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_faq(request, faq_id):
+    """ Delete an faq """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('delete_faq'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'FAQ deleted!')
+    return redirect(reverse('faq_page'))
