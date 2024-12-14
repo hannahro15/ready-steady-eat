410cbdb502124788c0f79269604183dd3661fb52
Hannah Olbrich <hannaholbrich@gmail.com>
2024-12-14 19:03:00 +0000

    Fix footer issue covering content

--- products/views.py
+++ products/views.py
@@ -86,7 +86,7 @@ def edit_product(request, product_id):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated product!')
-            return redirect('product_detail', args=[product.id])
+            return redirect('product_detail', product.id)
         else:
             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
     else:
