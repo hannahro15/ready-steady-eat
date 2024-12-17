1d20572aae78aa45f8983bec26ab3e7d0bcec5ac
Hannah Olbrich <hannaholbrich@gmail.com>
2024-12-17 13:12:11 +0000

    Style django forms better with widgets

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Faq, Contact
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput
 
 
 class FaqForm(forms.ModelForm):
@@ -10,6 +10,38 @@ class FaqForm(forms.ModelForm):
         fields = ['question', 'answer']
 
 class ContactForm(forms.ModelForm):
+    
     class Meta:
         model = Contact
         fields = ['firstName', 'lastName', 'email', 'phone', 'comments']
+        widgets = {
+            'firstName': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 1000px; margin-bottom: 10px;',
+                'placeholder': 'First Name'
+                }),
+
+            'lastName': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 1000px; margin-bottom: 10px;',
+                'placeholder': 'Last Name'
+                }),
+            
+            'email': EmailInput(attrs={
+                'class': "form-control", 
+                'style': 'max-width: 1000px; margin-bottom: 10px;',
+                'placeholder': 'Email'
+                }),
+
+            'phone': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 1000px; margin-bottom: 10px;',
+                'placeholder': 'Phone'
+                }),
+
+            'comments': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 1000px;',
+                'placeholder': 'Please enter any comments here'
+                }),
+        }
