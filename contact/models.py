6a4c2ee162d455e41a7dfdfcab8e569858ec48c7
Hannah Olbrich <hannaholbrich@gmail.com>
2024-12-31 12:21:40 +0000

    Change blank from True to False in contact models

--- contact/models.py
+++ contact/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=250, null=False, blank=True)
-    last_name = models.CharField(max_length=250, null=False, blank=True)
-    email = models.EmailField(max_length=250, null=False, blank=True)
-    phone = models.IntegerField(null=False, blank=True)
-    comments = models.TextField(max_length=250, null=False, blank=True)
+    first_name = models.CharField(max_length=250, null=False, blank=False)
+    last_name = models.CharField(max_length=250, null=False, blank=False)
+    email = models.EmailField(max_length=250, null=False, blank=False)
+    phone = models.IntegerField(null=True, blank=True)
+    comments = models.TextField(max_length=250, null=False, blank=False)
 
 
 class Faq(models.Model):
